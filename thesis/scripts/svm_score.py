60df021365588ddd55887d168bb5c4bc5208af1b
Carlo Rosso <carlorosso@Carlos-Air.lan>
2024-06-23 22:31:02 +0200

    aggiornamenti sulla tesi

---
+++ thesis/scripts/svm_score.py
@@ -0,0 +1,7 @@
+from sklearn.metrics import precision_score, recall_score, f1_score
+
+def score(y_true, y_pred):
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+    return precision, recall, f1
